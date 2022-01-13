487ef18ebcf14a66247e9088f6ff686b6e5bbf22
Lontchi Frank <fodjof6@gmail.com>
2022-01-13 06:35:31 +0100

    so did ome new stuff

--- src/pages/LandPage.jsx
+++ src/pages/LandPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import FirstViewDown from "./FirstViewDown";
 import Landing from "./Landing";
-import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax";
 import SecondView from "./SecondView";
 
 function LandPage() {
