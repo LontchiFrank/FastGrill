0c150288b7f810a63fc8433df5ad31d9a4ebaae8
LontchiFrank <fodjof6@gmail.com>
2022-06-30 22:44:53 +0100

    so great

--- src/pages/SecondView.jsx
+++ src/pages/SecondView.jsx
@@ -14,92 +14,94 @@ const inlineStyle = {
 };
 function SecondView() {
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
+    <div style={{ width: "100%", height: "90vh" }}>
       <Parallax bgImage={image2} blur={{ min: -2, max: 9 }}>
         <div
           style={{ height: 750, backgroundPosition: "center", width: "100%" }}
         >
           <div style={inlineStyle}>
-            <div
-              class="special-card d-flex"
-              style={{
-                width: "950px",
-                height: "450px",
-                background: "white",
-                borderRadius: "5px",
-              }}
-            >
+            <div className="holdspecial">
               <div
-                className="pic"
+                class="special-card d-flex"
                 style={{
-                  width: "45%",
-                  height: "100%",
-                }}
-              ></div>
-              <div
-                className="pic-txt"
-                style={{
-                  width: "55%",
-                  height: "100%",
+                  width: "950px",
+                  height: "450px",
+                  background: "white",
+                  borderRadius: "5px",
                 }}
               >
-                <div className="first-opt">
-                  <h2 className="title-txt pt-4">SPECIAL OFFER</h2>
-                </div>
-                <div className="second-opt px-4">
-                  <p class="card-text">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer. Nullam egestas aliquam est, vel elementum orci
-                    accumsan at. Suspendisse facilisis molestie ultrices Ut
-                    ultrices neque facilisis hendrerit rhoncus.This content is a
-                    little bit longer.
-                  </p>
-                  <div className="d-flex">
-                    <p className="m">Price: </p>
-                    <h4 className="off-1 ">6000frs</h4>
+                <div
+                  className="pic"
+                  style={{
+                    width: "45%",
+                    height: "100%",
+                  }}
+                ></div>
+                <div
+                  className="pic-txt"
+                  style={{
+                    width: "55%",
+                    height: "100%",
+                  }}
+                >
+                  <div className="first-opt">
+                    <h2 className="title-txt pt-4">SPECIAL OFFER</h2>
                   </div>
-                  <h3 className="on-1">5000frs</h3>
-                  <div className="colect d-flex">
-                    <div className="rating d-flex">
-                      <p style={{ marginRight: "6px" }}>Ratings:</p>
-                      <i class="fas fa-star"></i>
-                      <i class="fas fa-star"></i>
-                      <i class="fas fa-star"></i>
-                      <i class="fas fa-star"></i>
-                      <i class="fas fa-star-half-alt"></i>
+                  <div className="second-opt px-4">
+                    <p class="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer. Nullam egestas aliquam est, vel
+                      elementum orci accumsan at. Suspendisse facilisis molestie
+                      ultrices Ut ultrices neque facilisis hendrerit
+                      rhoncus.This content is a little bit longer.
+                    </p>
+                    <div className="d-flex">
+                      <p className="m">Price: </p>
+                      <h4 className="off-1 ">6000frs</h4>
                     </div>
-                    <div
-                      className="banner"
-                      style={{
-                        position: "absolute",
-                        top: "40%",
-                        left: "65%",
-
-                        // width: "50%",
-                        height: "60%",
-                      }}
-                    >
-                      <img
-                        src={image3}
-                        alt=""
+                    <h3 className="on-1">5000frs</h3>
+                    <div className="colect d-flex">
+                      <div className="rating d-flex">
+                        <p style={{ marginRight: "6px" }}>Ratings:</p>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                      </div>
+                      <div
+                        className="banner"
                         style={{
-                          zIndex: "1",
-                          width: "100%",
-                          height: "90%",
+                          position: "absolute",
+                          top: "40%",
+                          left: "65%",
+
+                          // width: "50%",
+                          height: "60%",
                         }}
-                      />
+                      >
+                        <img
+                          src={image3}
+                          alt=""
+                          style={{
+                            zIndex: "1",
+                            width: "100%",
+                            height: "90%",
+                          }}
+                        />
+                      </div>
                     </div>
+                    <p class="card-text-1">
+                      <small class="text-muted">
+                        <i
+                          class="fas fa-map-marker-alt"
+                          style={{ marginRight: "6px" }}
+                        ></i>
+                        Location 48 Spices at Sampit, Buea
+                      </small>
+                    </p>
                   </div>
-                  <p class="card-text-1">
-                    <small class="text-muted">
-                      <i
-                        class="fas fa-map-marker-alt"
-                        style={{ marginRight: "6px" }}
-                      ></i>
-                      Location 48 Spices at Sampit, Buea
-                    </small>
-                  </p>
                 </div>
               </div>
             </div>
